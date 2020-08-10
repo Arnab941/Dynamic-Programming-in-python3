2fae284cbe892fc5f45f70d52e56341bff080cc9
Arnab Nandy <69251923+Arnab941@users.noreply.github.com>
2020-08-11 02:46:01 +0530

    Add files via upload

---
+++ Minimum number of deletion in a string to make it palindrome Tabuler.py
@@ -0,0 +1,19 @@
+x='agbcba'
+y=x[::-1]
+m=len(x)
+n=m
+dp=[[-1 for i in range(n+1)] for j in range(m+1)]
+def lcs(m,n):
+    for i in range(m+1):
+        for j in range(n+1):
+            if i==0 or j==0:
+                dp[i][j]=0
+            elif x[i-1]==y[j-1]:
+                dp[i][j]=(1+dp[i-1][j-1])
+            else:
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+                
+    return dp[m][n]
+
+k=lcs(m,n)
+print(m-k) 
