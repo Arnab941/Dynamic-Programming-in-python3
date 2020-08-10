0e3e44437d40a6667ce6e36942979b4db45d7135
Arnab Nandy <69251923+Arnab941@users.noreply.github.com>
2020-08-11 02:10:46 +0530

    Add files via upload

---
+++ SCS (Shortest Common Supersequence) Length Tabuler.py
@@ -0,0 +1,19 @@
+x='geek'
+y='eke'
+m=len(x)
+n=len(y)
+dp=[[-1 for i in range(n+1)] for j in range(m+1)]
+def lcs(m,n):
+    for i in range(m+1):
+        for j in range(n+1):
+            if i==0 or j==0:
+                dp[i][j]=0
+            elif x[i-1]==y[j-1]:
+                dp[i][j]=(1+dp[i-1][j-1])
+            else:
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+                
+    return dp[m][n]
+
+k=m+n-lcs(m,n)
+print(k) 
