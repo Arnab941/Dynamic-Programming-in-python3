651661f6e6a28cac1bbdeddaa2e5ab360ba1f1ce
Arnab Nandy <69251923+Arnab941@users.noreply.github.com>
2020-08-11 10:24:52 +0530

    Add files via upload

---
+++ Printing SCS (Shortest common supersequence) Tabuler.py
@@ -0,0 +1,42 @@
+x='hello'
+y='geek'
+m=len(x)
+n=len(y)
+dp=[[-1 for i in range(n+1)] for j in range(m+1)]
+def lcs(m,n):
+    for i in range(m+1):
+        for j in range(n+1):
+            if i==0 or j==0:
+                dp[i][j]=0
+            elif x[i-1]==y[j-1]:
+                dp[i][j]=(1+dp[i-1][j-1])
+            else:
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+    
+    i1=m
+    j1=n
+    c=''           
+    while i1!=0 and j1!=0:
+        if x[i1-1]==y[j1-1]:
+            c+=(x[i1-1])
+            i1-=1
+            j1-=1
+        else:
+            if dp[i1-1][j1]>dp[i1][j1-1]:
+                c+=x[i1-1]
+                i1-=1
+            else:
+                c+=y[j1-1]
+                j1-=1
+        if i1==0 and j1!=0:
+            while j1!=0:
+                c+=y[j1-1]
+                j1-=1
+        elif i1!=0 and j1==0:
+             while i1!=0:
+                c+=x[i1-1]
+                i1-=1
+    return c[::-1]
+
+k=lcs(m,n)
+print(k)
